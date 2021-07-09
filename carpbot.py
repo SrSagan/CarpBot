9de6307864424bfc90cb22c1cee290b91757b6d9
SrSagan <santiago.daldex@gmail.com>
2021-07-09 20:44:47 -0300

    code

--- carpbot.py
+++ carpbot.py
@@ -27,51 +27,7 @@ if __name__ == '__main__': #se cargan todos los archivos
 #MUSICA
 #----------------------------------------------------------------------------------------------------------------------------
 
-'''@bot.command(
-	name='leave',
-	help='Sale del canal de voz',
-	brief='Sale del canal de voz'
-	)
-async def leave(ctx):
-	await ctx.voice_client.disconnect()
 
-
-@bot.command(pass_context=True,
-	name='play',
-	help='Reproduce un link de youtube',
-	brief='Reproduce musica'
-	)
-async def play(ctx, *url):
-
-	await ctx.send("estoy aqui")
-	ytlinks.append(url)
-
-	for links in ytlinks:
-		print(links)
-		author = ctx.message.author
-		channel = author.voice.channel
-		vc = await channel.connect()
-		video = new(links[0])
-		audio = video.getbestaudio()
-		filename = audio.download()
-		vc.play(discord.FFmpegPCMAudio(str(video.title)+'.webm'))
-		vc.is_playing()
-		#os.system("rm"+' '+'"'+str(video.title)+'"'+'.webm')
-	await ctx.send("estoy aca")
-	
-	
-
-@bot.command(pass_context=True,
-	name='queue',
-	help='Muestra las canciones en la lista de reproduccion',
-	brief='Muestra la queue'
-	)
-async def queue(ctx):
-
-	for url in ytlinks:
-		print(url)
-		v = new(url[0])
-		await ctx.send(str(v.title))'''
 
 print("bot iniciado")
 bot.run(TOKEN)
