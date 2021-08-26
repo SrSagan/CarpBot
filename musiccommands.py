00c5e8159a9eccf62616c02562bcb2c643d60301
SrSagan <santiago.daldex@gmail.com>
2021-08-25 21:03:08 -0300

    music

--- musiccommands.py
+++ musiccommands.py
@@ -28,12 +28,19 @@ class music(commands.Cog):
                       )
     async def play(self, ctx, *url):
 
-        for link in url:
-            a.add_links(link)
+        author = ctx.message.author
+        channel = author.voice.channel
 
         links = a.get_links()
+        for link in url:
+
+            if link in links:
+                await ctx.send("El link ya esta en la lista")
+            else:
+                a.add_links(link)
 
         for link in links:
+
             ydl_opts = a.get_yld_opts()  # opciones de descarga guardadas en datos
             ydl = youtube_dl.YoutubeDL(ydl_opts)
             ydl.download([link])
@@ -41,23 +48,19 @@ class music(commands.Cog):
             songNum = ydl_opts["outtmpl"]
             x = songNum.find(".mp3")
             songNum = int(songNum[:x])
-            print(songNum)
             songNum = songNum+1
             ydl_opts["outtmpl"] = str(songNum)+".mp3"
+
             a.set_yld_opts(ydl_opts)
+            
+        voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        if voice_client == None:  # si no esta conectado
+            vc = await channel.connect()
 
-            author = ctx.message.author
-            channel = author.voice.channel
-
-            voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
-
-            if voice_client == None:
-                vc = await channel.connect()  # entra en la llamada
-
-            #vc.play(discord.FFmpegPCMAudio(str(songNum-1)+".mp3"))
-            if vc.is_playing() == False:
-                vc.play(discord.FFmpegPCMAudio(str(songNum-1)+".mp3"))
-                
+        links = a.get_links()
+        
+        for link in links:
+            vc.play(discord.FFmpegPCMAudio(str(songNum-1)+".mp3"))
 
 
 def setup(bot):
