d634249a6e90483d1f6dfe228e33007e85c93e3a
SrSagan <santiago.daldex@gmail.com>
2021-08-27 20:36:15 -0300

    song

--- music.py
+++ music.py
@@ -4,6 +4,10 @@ from discord.ext import commands
 from discord import FFmpegPCMAudio
 import time
 import asyncio
+import youtube_dl
+import data
+
+a = data.datos()
 
 
 class music:
@@ -12,11 +16,13 @@ class music:
         names = []
         files = []
         index = 0
+        status = False
 
         self.links = links
         self.names = names
         self.files = files
         self.index = index
+        self.status = status
 
 #------------------LINKS------------------#
 
@@ -48,7 +54,15 @@ class music:
         return self.index
 
     def set_index(self, index):
-        self.inxdex = index
+        self.index = index
+
+#-----------------STATUS------------------#
+
+    def get_status(self):
+        return self.status
+
+    def set_status(self, status):
+        self.status = status
 
 #-----------------GENERAL-----------------#
 
@@ -58,7 +72,39 @@ class music:
         self.files = []
         self.index = 0
 
+#------------YOUTUBE DOWNLOAD-------------#
+
+    async def youtube_download(self, ctx, url):
+        for link in url:
+
+            if link in self.links:
+                await ctx.send("El link ya esta en la lista")
+            else:
+                self.links.append(link)
+
+                ydl_opts = a.get_yld_opts()  # opciones de descarga guardadas en datos
+                ydl = youtube_dl.YoutubeDL(ydl_opts)
+                ydl.download([link])
+
+                info_dict = ydl.extract_info(
+                    link, download=False)  # guarda el nombre
+                    
+                self.names.append(info_dict.get('title', None))
+
+                songNum = ydl_opts["outtmpl"]
+                x = songNum.find(".mp3")
+                songNum = int(songNum[:x])
+                self.files.append(str(songNum)+".mp3")
+
+                songNum = songNum+1
+                # descarga todos los links y cambia el nombre del proximo archivo a descargar
+                ydl_opts["outtmpl"] = str(songNum)+".mp3"
+
+                a.set_yld_opts(ydl_opts)
+
+
 #--------------MUSIC PLAYER---------------#
+
 
     async def play(self, vc, ctx):
 
@@ -66,22 +112,23 @@ class music:
 
             while True:  # si esta reproduciendo no hace nada y espera
                 if vc.is_playing() == False:
-                    break
+                    if vc.is_paused() == False:
+                        break
                 await asyncio.sleep(0.25)
 
             # si termina la queue frena el loop
-            if self.index+1 > len(self.links):
+            if self.index+1 > len(self.links) or self.status == False:
                 await ctx.send("Queue over")
                 break
 
             embed = discord.Embed(
                 title="Now Playing", color=0x3498DB, description=str(self.names[self.index]))
             await ctx.send(embed=embed)  # muestra que esta reproduciendo
-
+            await ctx.send("playing index:"+str(self.index))
             vc.play(discord.FFmpegPCMAudio(
                 self.files[self.index]))  # reproduce
 
             # elimina el archivo anterior para ahorrar espacio
-            if self.index != 0:
-                os.system("rm "+self.files[self.index-1])
+            if self.index >= 2:
+                os.system("rm "+self.files[self.index-2])
             self.index = self.index+1  # sube el contador
--- musiccommands.py
+++ musiccommands.py
@@ -30,6 +30,8 @@ class music(commands.Cog):
         os.system("rm *.mp3")
         b.reset_all()
 
+#---------------------------------------------------------PLAY----------------------------------------------------------#
+
     @commands.command(pass_context=True,  # reproduce musica
                       aliases=['p', 'pl'],
                       name='play',
@@ -51,45 +53,19 @@ class music(commands.Cog):
 
         if voice_client != None:
 
-            for link in url:
-
-                if link in links:
-                    await ctx.send("El link ya esta en la lista")
-                else:
-                    links.append(link)
-                    b.set_links(links)
-
-                    ydl_opts = a.get_yld_opts()  # opciones de descarga guardadas en datos
-                    ydl = youtube_dl.YoutubeDL(ydl_opts)
-                    ydl.download([link])
-
-                    info_dict = ydl.extract_info(
-                        link, download=False)  # guarda el nombre
-                    names = b.get_names()
-                    names.append(info_dict.get('title', None))
-                    b.set_names(names)
-
-                    songNum = ydl_opts["outtmpl"]
-                    songs.append(str(songNum))
-                    x = songNum.find(".mp3")
-                    songNum = int(songNum[:x])
-                    songs = b.get_files()
-                    songs.append(str(songNum)+".mp3")
-                    b.set_files(songs)
-                    songNum = songNum+1
-                    # descarga todos los links y cambia el nombre del proximo archivo a descargar
-                    ydl_opts["outtmpl"] = str(songNum)+".mp3"
-
-                    a.set_yld_opts(ydl_opts)
+            await b.youtube_download(ctx, url)
 
             vc = ctx.voice_client  # si no esta reproduciendo, comienza a reproducir
             if vc.is_playing() == False:
+                b.set_status(True)
                 await b.play(vc, ctx)
 
         # SI NO ESTA CONECTADO SE TIENE QUE CONECTAR Y REPRODUCIR >:(((((((((( uuh acabo de caer si esta conectado y no esta reproduciendo si ;-;
         # WENO SE ARREGLA DESPOIS
         else:
-
+            
+            await b.youtube_download(ctx, url)
+            '''
             for link in url:
                 if link in links:
                     await ctx.send("El link ya esta en la lista")
@@ -115,13 +91,16 @@ class music(commands.Cog):
                     b.set_files(songs)
                     songNum = songNum+1
                     ydl_opts["outtmpl"] = str(songNum)+".mp3"
-                    a.set_yld_opts(ydl_opts)
+                    a.set_yld_opts(ydl_opts)'''
 
             await channel.connect()
 
             vc = ctx.voice_client
             if vc.is_playing() == False:  # si no esta reproduciendo comienza a reproducir (no hace falta pero weno)
+                b.set_status(True)
                 await b.play(vc, ctx)
+
+#---------------------------------------------------------QUEUE----------------------------------------------------------#
 
     @commands.command(
         aliases=['q'],
@@ -144,6 +123,8 @@ class music(commands.Cog):
             title="Queue", color=0x3498DB, description=data)
         await ctx.send(embed=embed)
     
+#---------------------------------------------------------STOP----------------------------------------------------------#
+
     @commands.command(
         aliases=['s'],
         name='stop',
@@ -152,8 +133,11 @@ class music(commands.Cog):
     )
     async def stop(self, ctx):
         vc = ctx.voice_client
+        b.set_status(False)
         vc.stop()
-    
+
+#---------------------------------------------------------PAUSE----------------------------------------------------------#
+
     @commands.command(
         aliases=['ps'],
         name='pause',
@@ -167,6 +151,8 @@ class music(commands.Cog):
         else:
             vc.pause()
 
+#---------------------------------------------------------RESUME----------------------------------------------------------#
+
     @commands.command(
         aliases=['r'],
         name='resume',
@@ -176,9 +162,39 @@ class music(commands.Cog):
     async def resume(self, ctx):
         vc = ctx.voice_client
         if vc.is_paused() == True:
-            vc.stop()
+            vc.resume()
         else:
             await ctx.send("Audio not paused")
+    
+#---------------------------------------------------------NEXT----------------------------------------------------------#
+
+    @commands.command(
+        aliases=['n', 'skip'],
+        name='next',
+        help='Salta la cancion',
+        brief='Salta la musica'
+    )
+    async def next(self, ctx):
+        vc = ctx.voice_client
+        index = b.get_index()
+        index = index
+        b.set_index(index)
+        vc.stop()
+
+#---------------------------------------------------------BACK----------------------------------------------------------#
+
+    @commands.command(
+        aliases=['b'],
+        name='back',
+        help='Vuelve a la anterior cancion',
+        brief='Vuelve la musica'
+    )
+    async def back(self, ctx):
+        vc = ctx.voice_client
+        index = b.get_index()
+        index = index-2
+        b.set_index(index)
+        vc.stop()
     
     
 
