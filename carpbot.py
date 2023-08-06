61715ef3f2547b541b4d74c5ae93d0b813af7ffa
SrSagan <santiago.daldex@gmail.com>
2023-08-06 02:08:45 -0300

    omg logs

--- carpbot.py
+++ carpbot.py
@@ -1,10 +1,30 @@
 import os
+import sys
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import data
 import asyncio
 d = data.datos()
+import logging
+import time
+
+#all logging
+
+timestr = time.strftime("%d-%Y-%m--%H:%m:%S")
+
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+
+fileHandler = logging.FileHandler("{0}/{1}.log".format("logs", timestr))
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+consoleHandler = logging.StreamHandler(sys.stdout)
+consoleHandler.setFormatter(logFormatter)
+rootLogger.addHandler(consoleHandler)
+
+#till here
 
 intents = discord.Intents.default()
 intents.message_content = True
