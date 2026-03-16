484b5023a172e255e780d301043bc7628aac6c4a
Ramiro Mendoza <ramimendoza13@gmail.com>
2026-03-16 10:57:07 -0300

    refactor:(musica): deprecando código legacy

---
+++ legacy/musica/server.py
@@ -0,0 +1,26 @@
+import json
+import os
+import data
+import musica.servermanager
+
+sm = musica.servermanager.serverManager()
+
+
+class song:
+    def __init__(self, name, link, length, type):
+        self.name = name
+        self.link = link
+        self.length = length
+        self.type = type
+
+
+class server:
+    def __init__(self, id):
+        self.id = id
+        self.cplaying = 0
+        self.ptime = None
+        self.time = None
+        self.status = False
+        self.tlength = 0
+        self.looping = 0
+        self.songs = []
