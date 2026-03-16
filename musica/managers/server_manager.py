a0f9576049953610a418874b3eb230b3f97a062f
Ramiro <ramimendoza13@gmail.com>
2026-03-15 22:10:54 -0300

    refactor(server-manager): convert _servers to a class variable and update access methods

--- musica/managers/server_manager.py
+++ musica/managers/server_manager.py
@@ -1,23 +1,36 @@
-from typing import Dict, List, Optional
+from typing import ClassVar, Dict, List, Optional
+
 from musica.models.server import Server
 
 
 class ServerManager:
-    def __init__(self):
-        self._servers = Dict[int, Server] = {}
+    _servers: ClassVar[Dict[int, Server]] = {}
 
     def add_server(self, server: Server):
-        self._servers[server.id] = server
+        ServerManager._servers[server.id] = server
+
+    def ensure_server(self, guild_id: int, name: str = "") -> Server:
+        server = self.get_server(guild_id)
+        if server is None:
+            server = Server(id=guild_id, name=name)
+            self.add_server(server)
+        return server
 
     def get_server(self, id: int) -> Optional[Server]:
-        return self._servers.get(id)
+        return ServerManager._servers.get(id)
 
     def remove_server(self, id: int) -> bool:
-        deleted_server = self._servers.pop(id, None)
+        deleted_server = ServerManager._servers.pop(id, None)
         return deleted_server is not None
 
+    def clear(self, id: int) -> bool:
+        return self.remove_server(id)
+
     def list_servers(self) -> List[Server]:
-        return list(self._servers.values())
+        return list(ServerManager._servers.values())
 
     def exists(self, id: int) -> bool:
-        return id in self._servers
+        return id in ServerManager._servers
+
+    def get_server_store(self) -> Dict[int, Server]:
+        return ServerManager._servers
