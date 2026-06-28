f7cb56ee84bea226a82cb8ca655cfb90295e5d98
Ramiro Nicolas Mendoza <rmendoza@MacBook-Pro-de-Ramiro.local>
2026-06-28 15:06:30 -0300

    fix: mensajes de bola 8

--- commands/general/eightball.py
+++ commands/general/eightball.py
@@ -5,6 +5,7 @@ from commands.base.base_command import UserCommand
 from discord.ext import commands
 import lenguajes as len
 
+
 class EightBallCommand(UserCommand):
     """Comando de bola 8 - Disponible para todos."""
 
@@ -16,23 +17,18 @@ class EightBallCommand(UserCommand):
         """Responde a una pregunta con una respuesta aleatoria de bola 8."""
         self.log_command_user(ctx, "bola8")
 
-        embed = discord.Embed(
-            title="🎱 Bola 8 mágica.",
-            color=0x3498DB
-        )
-        
+        embed = discord.Embed(title="🎱 Bola 8 mágica.", color=0x3498DB)
+
         texto_pregunta = " ".join(args).strip()
         embed.add_field(
-            name=len.p_r[self.get_language(ctx)],
-            value=texto_pregunta,
-            inline=False
+            name=len.p_r[self.get_language(ctx)][0], value=texto_pregunta, inline=False
         )
 
         respuestas = len.Respuestas8ball[self.get_language(ctx)]
         embed.add_field(
-            name=len.p_r[self.get_language(ctx)],
+            name=len.p_r[self.get_language(ctx)][1],
             value=random.choice(respuestas),
-            inline=False
+            inline=False,
         )
 
         await ctx.send(embed=embed)
